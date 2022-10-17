71272eadcee482adbdf00112a5c04b5e0f3090ea
jesperkeden <jesper.keden@hotmail.com>
2022-10-17 15:25:29 +0200

    Update Program.cs

--- TjuvarOchPoliser/TjuvarOchPoliser/Program.cs
+++ TjuvarOchPoliser/TjuvarOchPoliser/Program.cs
@@ -6,6 +6,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-    Console.WriteLine("KOKO!");
+        Console.WriteLine("KOKO!");
+        Console.WriteLine("KOKO!");
     }
 }
