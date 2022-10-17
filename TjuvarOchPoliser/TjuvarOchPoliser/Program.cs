45a75f28dffffd1bbbff73e3e5638b0989c88fe7
Gr2dd3 <gradde23@gmail.com>
2022-10-17 15:00:36 +0200

    Update Program.cs

--- TjuvarOchPoliser/TjuvarOchPoliser/Program.cs
+++ TjuvarOchPoliser/TjuvarOchPoliser/Program.cs
@@ -6,12 +6,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Jesper testar GIT!");
 
-        Console.WriteLine("Gr2dd3 också");
-
-        Console.WriteLine("Hanna & Jonna, where are you?");
-
-        Console.WriteLine("hejhej");
     }
 }
