9c6c78c34d1442905bb1e23d51f18a0e9217ef67
Gr2dd3 <gradde23@gmail.com>
2022-10-20 14:55:33 +0200

    Börjat med Prison

--- TjuvarOchPoliser/TjuvarOchPoliser/City.cs
+++ TjuvarOchPoliser/TjuvarOchPoliser/City.cs
@@ -126,7 +126,7 @@ namespace TjuvarOchPoliser
                 {
                     RobbedCounter++;
                 }
-                
+
 
             }
             // Beslagta
@@ -170,20 +170,47 @@ namespace TjuvarOchPoliser
         }
     }
 
+
+
     internal class Prison
     {
-        public void DrawPrison()
-        {
-            int[,] prison = new int[10, 10];
+        public List<Person> Prisoners { get; set; }
+        public int PrisonCounter { get; set; }
+        public Person[,] Matrix { get; set; }
 
-            for (int rows = 0; rows < prison.GetLength(0); rows++)
+        public Prison()
+        {
+            Matrix = new Person [10, 10];
+        }
+
+
+        public void DrawPrison(Prison prison)
+        {
+            
+
+            for (int rows = 0; rows < Matrix.GetLength(0); rows++)
             {
-                for (int cols = 0; cols < prison.GetLength(1); cols++)
+                for (int cols = 0; cols < Matrix.GetLength(1); cols++)
                 {
-                    Console.Write(prison[rows, cols]);
+                    Console.Write(Matrix[rows, cols] == null ? " " : Matrix[rows, cols]);
                 }
                 Console.WriteLine();
             }
         }
+
+
+
+        //public void RunPrison(Person person, City city)
+        //{
+        //    for (int i = 0; i < Prisoners.Count; i++)
+        //    {
+        //        if (((Thief)person).Loot.Count == 4)
+        //        {
+        //            PrisonCounter = 40;
+
+        //            PrisonCounter--;
+        //        }
+        //    }
+        //}
     }
 }
